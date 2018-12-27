642e571ea4656014306a35db979e5eb663c73efd
yeyun <644362887@qq.com>
2018-12-27 17:55:02 +0800

    Update params.simple.php
    config

--- config/params.simple.php
+++ config/params.simple.php
@@ -3,10 +3,10 @@
 return [
     // 微信
     'wecaht' => [
-        'token'  => 'bVCDd7LF7CFk4miaQohWEDeZjDzbWvhq',
-        'aes'    => 'TAkuC2sLChq5XUDLOdckIf7Xl48VJiqrQcuxs9jv4yW',
-        'appid'  => 'wx00e0860a4dd09082',
-        'secret' => '25de1144e02b1a5cf4d52d05f76bb6d1',
-        'name'   => 'touchsprite',
+        'token'  => '你猜',
+        'aes'    => '你猜',
+        'appid'  => '你猜',
+        'secret' => '你猜',
+        'name'   => '你猜',
     ],
 ];
